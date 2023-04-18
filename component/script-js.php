44bdcc2dffbea0a7ce9d984055d3fb88a35464a3
jamornP <100953989+jamornP@users.noreply.github.com>
2023-04-18 14:02:16 +0700

    google นับจำนวนผู้เข้าชม

--- component/script-js.php
+++ component/script-js.php
@@ -129,7 +129,15 @@ $(function(){
  
 });
 </script>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-TQGB3JJ9SF"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
+  gtag('config', 'G-TQGB3JJ9SF');
+</script>
 
 
 
